003af640f8bd5e0d50faa276dd7dcb4ce6f5d829
Alex Monday <monday15@gmx.com>
2019-06-16 20:06:46 +0500

    gtk2: Merge dt/lt assets into one file, adjust render script
    - Update assets list
    - Remove unused external assets files
    - Add sources for compact assets

--- src/gtk2/render-assets.sh
+++ src/gtk2/render-assets.sh
@@ -8,17 +8,12 @@ ASSETS_DIR="assets"
 INDEX="assets.txt"
 
 for i in `cat $INDEX | grep -v '^#'`
-do 
-#if [ -f $ASSETS_DIR/$i.png ]; then
-#    echo $ASSETS_DIR/$i.png exists.
-#else
-#    echo
+do
     echo Rendering $ASSETS_DIR/$i.png
     $INKSCAPE --export-id=$i \
               --export-id-only \
               --export-background-opacity=0 \
               --export-png=$ASSETS_DIR/$i.png $SRC_FILE >/dev/null \
     && $ZOPFLIPNG -ym $ASSETS_DIR/$i.png $ASSETS_DIR/$i.png
-#fi
 done
 exit 0
