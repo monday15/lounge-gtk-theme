45ae9860369c6895ad7b92af73049cafa790e465
Alex Monday <monday15@gmx.com>
2019-06-16 21:01:14 +0500

    xfwm: Merge dt/lt assets into one file, adjust render script

--- src/xfwm4/xfwm4-render-assets.sh
+++ src/xfwm4/xfwm4-render-assets.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
 
 INKSCAPE="/usr/bin/inkscape"
+ZOPFLIPNG="/usr/bin/zopflipng"
 
 SRC_FILE="xfwm4-assets.svg"
-ASSETS_DIR="."
+ASSETS_DIR="assets"
 INDEX="xfwm4-assets.txt"
 
 for i in `cat $INDEX | grep -v '^#'`
@@ -11,6 +12,7 @@ do
     echo Rendering $ASSETS_DIR/$i.png
     $INKSCAPE --export-id=$i \
               --export-id-only \
-              --export-png=$ASSETS_DIR/$i.png $SRC_FILE >/dev/null
+              --export-png=$ASSETS_DIR/$i.png $SRC_FILE >/dev/null \
+    && $ZOPFLIPNG -ym $ASSETS_DIR/$i.png $ASSETS_DIR/$i.png
 done
 exit 0
