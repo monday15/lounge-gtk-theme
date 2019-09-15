6b6f07305a0284712de6419602d9ed93576efe14
Alex Monday <monday15@gmx.com>
2019-09-15 03:00:31 +0100

    gtk2: Restyle assets
    - New style;
    - Add flat assets;
    - Add tone assets;
    - Add optional assets for fancy scale style;
    - Add optional handle assets for scrollbars.

---
+++ src/gtk2/render-assets-extra.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+INKSCAPE="/usr/bin/inkscape"
+ZOPFLIPNG="/usr/bin/zopflipng"
+
+SRC_FILE="assets-extra.svg"
+ASSETS_DIR="assets"
+INDEX="assets-extra.txt"
+
+for i in `cat $INDEX | grep -v '^#'`
+do
+    echo Rendering $ASSETS_DIR/$i.png
+    $INKSCAPE --export-id=$i \
+              --export-id-only \
+              --export-background-opacity=0 \
+              --export-png=$ASSETS_DIR/$i.png $SRC_FILE >/dev/null \
+    && $ZOPFLIPNG -ym $ASSETS_DIR/$i.png $ASSETS_DIR/$i.png
+done
+exit 0
